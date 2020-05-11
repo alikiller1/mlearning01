bc79bbf36ea5bee956be2d36b8f686dee16e60cd
alikiller1 <liu2011321@sina.com>
2020-05-11 16:56:15 +0800

    aaa

--- test04.py
+++ test04.py
@@ -3,11 +3,25 @@
 # @Time    : 2020/5/11 11:58
 # @Author  : liuqh
 # @Software: PyCharm
+from utils import logger
+
+
+def f1(a,b):
+    """
+    求和方法
+    :param a:float a
+    :param b:float a
+    :return:a+b
+    """
+    sum=a+b;
+    logger.info("sum=%d" %sum)
+    return sum
 
 if __name__ == '__main__':
-    a=1;
-    b=2;
-    if ( (a==1 and b==2)):
-        print(3)
+    a = 2;
+    b = 2;
+    if (not a == 1) and b == 2:
+        logger.info(3)
     else:
         print(4)
+    f1(1,2)
